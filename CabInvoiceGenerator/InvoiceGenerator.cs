f80290d5aa491fe2088f00076c48f39f10c70d89
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-04 23:03:45 +0530

    [Madhavee] introduce premium rides

--- CabInvoiceGenerator/InvoiceGenerator.cs
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -8,6 +8,7 @@ namespace CabInvoiceGenerator
 {
     public class InvoiceGenerator
     {
+        public RideType type;
         RideRepository rideRepository = new RideRepository();
         /// <summary>
         /// Declaring some variable
@@ -18,11 +19,28 @@ namespace CabInvoiceGenerator
         /// <summary>
         /// Creating a constructor
         /// </summary>
-        public InvoiceGenerator()
+        public InvoiceGenerator(RideType type)
         {
-            this.MINIMUM_COST_PER_KM = 10;
-            this.COST_PER_TIME = 1;
-            this.MINIMUM_FARE = 5;
+            this.type = type;
+            try
+            {
+                if(this.type.Equals(RideType.NORMAL))
+                {
+                    this.MINIMUM_COST_PER_KM = 10;
+                    this.COST_PER_TIME = 1;
+                    this.MINIMUM_FARE = 5; 
+                }
+                if(this.type.Equals(RideType.PREMIUM))
+                {
+                    this.MINIMUM_COST_PER_KM = 10;
+                    this.COST_PER_TIME = 1;
+                    this.MINIMUM_FARE = 5;
+                }
+            }
+            catch(Exception)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_TYPE, "Invalid ride type");
+            }
         }
         /// <summary>
         /// Creating method for calculating Totalfare based on distance and time
--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -10,7 +10,7 @@ namespace CabInvoiceGenerator
     {
         static void Main(string[] args)
         {
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             Console.WriteLine(invoiceGenerator.CalculateFare(5,6));
             Ride[] rides = { new Ride(3, 25), new Ride(0.5, 10) };
             InvoiceSummary actual = invoiceGenerator.CalculateFare(rides);
